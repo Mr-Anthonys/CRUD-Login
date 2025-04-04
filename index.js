7e400b5d4729907f2109fd9435b2eff9041c0319
oiDreas <andreasanthony92@gmail.com>
2025-04-04 16:03:43 -0300

    .

---
+++ index.js
@@ -0,0 +1,9 @@
+function validateFields() {
+    //pegar o valor do campo email
+    //verificar se o email não é vazio e se o email é valido
+    //se verdadeiro, então habilitar o botão de recuperar senha
+    //se falso, então desabilitar o botão de recuperar senha
+    email = document.getElementById("email").value;
+    if (!email)
+
+}
