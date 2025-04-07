46068d474118b59cd3965007c1514df31d232333
oiDreas <andreasanthony92@gmail.com>
2025-04-07 18:36:24 -0300

    .

--- index.js
+++ index.js
@@ -1,9 +1,49 @@
+//FUNÇÃO PRINCIPAL
 function validateFields() {
-    //pegar o valor do campo email
-    //verificar se o email não é vazio e se o email é valido
-    //se verdadeiro, então habilitar o botão de recuperar senha
-    //se falso, então desabilitar o botão de recuperar senha
-    email = document.getElementById("email").value;
-    if (!email)
+    toogleButtonDisable();
+    toggleEmailErrors();
+}
 
+function isEmailValid() {
+    const email = document.getElementById("email").value;
+    if (!email) {
+        return false;
+    }
+    return validateEmail(email);
+}
+
+function toggleEmailErrors() {
+    const email = document.getElementById("email").value;
+    if (!email) {
+        document.getElementById('email-required-error').style.display = 'block';
+    } else {
+        document.getElementById('email-required-error').style.display = 'none';
+    }
+
+    if (validateEmail(email)) {
+        document.getElementById('email-invalid-error').style.display = 'block';
+    } else {
+        document.getElementById('email-invalid-error').style.display = 'none';
+    }
+}
+
+function toogleButtonDisable() {
+    const emailValid = isEmailValid();
+    document.getElementById('recover-password-button').disabled = !emailValid;
+
+    const passwordValid = isPasswordValid();
+    document.getElementById('login-button').disabled = !emailValid || !passwordValid;
+}
+
+function isPasswordValid() {
+    const password = document.getElementById('password').value;
+    if (!password) {
+        return false;
+    }
+    return true;
+}
+
+//FUNÇÃO QUE VALIDA EMAIL
+function validateEmail(email) {
+    return /\S+@\S+\.\S+/.test(email);
 }
